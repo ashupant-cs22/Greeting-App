04c9f8b9f133f09b682c2057e05f55f094717f95
Ashu Pant <108819915+ashupant-cs22@users.noreply.github.com>
2022-09-20 21:26:23 +0530

    Delete mygreetingapp directory

--- mygreetingapp/repository/GreetingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.mygreetingapp.repository;
-
-import com.bridgelabz.mygreetingapp.model.Greeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GreetingRepo extends JpaRepository<Greeting, Long>  {
-}
