2f468ea64b7b65fff128640c7a29a4ab05dd34fc
Ashu Pant <108819915+ashupant-cs22@users.noreply.github.com>
2022-09-20 21:27:20 +0530

    Added : My Greeting App

---
+++ mygreetingapp/MygreetingappApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.mygreetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygreetingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MygreetingappApplication.class, args);
+    }
+
+}
